125c2ceff2089e1f447d566926e85c3b01f3a847
vikram <svikram9500@gmail.com>
2021-02-18 11:41:22 +0530

    third commit

---
+++ app/src/main/java/com/example/productrecycler/Registerr.java
@@ -0,0 +1,14 @@
+package com.example.productrecycler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registerr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registerr);
+    }
+}
